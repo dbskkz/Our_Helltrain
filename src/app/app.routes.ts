71756e55b769f14218c9a5f3d238ae8f91ad07a4
yoyo <yoyo0906915726@gmail.com>
2026-05-11 10:10:22 +0800

    0511 新增後台路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AnnouncementComponent } from './@component/announcement/announcement.component';
+import { BackIndexComponent } from './@component/back-index/back-index.component';
+import { BackUserComponent } from './@component/back-user/back-user.component';
+import { BackProductComponent } from './@component/back-product/back-product.component';
+import { ReportComponent } from './@component/report/report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //後台
+  {path: 'announcement', component:AnnouncementComponent},//公告
+  {path: 'back_index', component:BackIndexComponent},//首頁
+  {path: 'back_product', component:BackProductComponent},//商品管理頁
+  {path: 'back_user', component:BackUserComponent},//用戶管理
+  {path: 'report', component:ReportComponent}//爭議處理
+
+  //前台
+
+];
