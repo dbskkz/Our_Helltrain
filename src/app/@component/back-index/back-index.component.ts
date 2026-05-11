1a2a948587bb8d98ae6a8fb5616e0b530feb61b3
dbskkz <dbskkz1012>
2026-05-11 09:25:32 +0800

    5/11 刪除現有component中的測試檔(spec.ts)

--- src/app/@component/back-index/back-index.component.ts
+++ src/app/@component/back-index/back-index.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-back-index',
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './back-index.component.html',
   styleUrl: './back-index.component.scss'
 })
 export class BackIndexComponent {
-
+newuser=0;
+dispute=0;
 }
---
+++ src/app/@component/back-sider/back-sider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-sider',
+  imports: [],
+  templateUrl: './back-sider.component.html',
+  styleUrl: './back-sider.component.scss'
+})
+export class BackSiderComponent {
+
+}
