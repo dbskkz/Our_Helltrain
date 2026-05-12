504a6e843483ef8007acea4f236e7caffb3a91de
alice-0528 <cat34000258@gmail.com>
2026-05-12 18:57:01 +0800

    new component_orderInformation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,17 @@ import { BackUserComponent } from './@component/back-user/back-user.component';
 import { BackProductComponent } from './@component/back-product/back-product.component';
 import { ReportComponent } from './@component/report/report.component';
 import { ProfileSettingsComponent } from './@component/profile-settings/profile-settings.component';
+import { OrderInformationComponent } from './@component/order-information/order-information.component';
 
 export const routes: Routes = [
   //後台
-  {path: 'announcement', component:AnnouncementComponent},//公告
-  {path: 'back_index', component:BackIndexComponent},//首頁
-  {path: 'back_product', component:BackProductComponent},//商品管理頁
-  {path: 'back_user', component:BackUserComponent},//用戶管理
-  {path: 'report', component:ReportComponent},//爭議處理
+  { path: 'announcement', component: AnnouncementComponent }, //公告
+  { path: 'back_index', component: BackIndexComponent }, //首頁
+  { path: 'back_product', component: BackProductComponent }, //商品管理頁
+  { path: 'back_user', component: BackUserComponent }, //用戶管理
+  { path: 'report', component: ReportComponent }, //爭議處理
 
   //前台
-  {path: 'profile_settings',component:ProfileSettingsComponent}//個人設定
+  { path: 'profile_settings', component: ProfileSettingsComponent }, //個人設定
+  { path: 'order_information', component: OrderInformationComponent }, //訂單資料
 ];
