da130a1c69bb97b3405b134e8a776c98ae0a6ec0
alice-0528 <cat34000258@gmail.com>
2026-05-13 16:33:32 +0800

    0513_order-component update

--- src/app/@component/order-information/order-information.component.ts
+++ src/app/@component/order-information/order-information.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import {
+  CircleCheckBig,
+  LucideAngularModule,
+  MessageCircleMore,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-order-information',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './order-information.component.html',
-  styleUrl: './order-information.component.scss'
+  styleUrl: './order-information.component.scss',
 })
 export class OrderInformationComponent {
+  // Declare icon
+  readonly CircleCheckBig = CircleCheckBig;
+  readonly MessageCircleMore = MessageCircleMore;
 
+  //tabs
+  currentTab = '全部'; // 預設選中
+
+  // 分頁變數
+  currentPage = 1;
+  pageSize = 5;
+  totalElements = 0;
+  totalPages = 3;
+
+  // 存放當前頁要顯示的資料
+  orders: any[] = [];
+
+  changeTab(tabName: string) {
+    this.currentTab = tabName;
+    this.currentPage = 1; // 切換分類時，頁碼歸零
+    this.fetchOrders();
+  }
+
+  buyAgain() {}
+
+  chat() {}
+
+  fetchOrders() {}
+
+  // 點擊頁碼
+  goToPage(page: number) {
+    if (page < 1 || page > this.totalPages) return;
+    this.currentPage = page;
+    this.fetchOrders();
+  }
 }
