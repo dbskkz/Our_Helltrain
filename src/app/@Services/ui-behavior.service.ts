ff6f5d6e1657c1059416e0e2c2148023c2b6d54a
Pei Lin <a0936856077@gmail.com>
2026-05-16 23:34:32 +0800

    完成 ui-behavior service 中彈出面板的邏輯，並引用至 product-list component

--- src/app/@Services/ui-behavior.service.ts
+++ src/app/@Services/ui-behavior.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class UiBehaviorService {
 
   constructor() { }
+
+  // 點擊彈出視窗
+  togglePanel
+  (
+    event:Event,
+    panelState:Record<string, boolean>,
+    panel:string
+  )
+  {
+    event.stopPropagation();
+    panelState[panel] = !panelState[panel];
+  }
+
+  closeAll(panelState: Record<string, boolean>): void {
+
+    Object.keys(panelState).forEach(key => {
+
+      panelState[key] = false;
+
+    });
+
+  }
 }
