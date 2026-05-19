9f199f98f5c0bc4ddaf7cb73a1d8c01f7fb8a53e
Yujay-666 <sadghf231564@gmail.com>
2026-05-19 15:27:00 +0800

    上架商品頁大致完成

--- src/app/@component/launch-product/launch-product.component.ts
+++ src/app/@component/launch-product/launch-product.component.ts
@@ -1,5 +1,32 @@
 import { Component } from '@angular/core';
 
+interface ProductState {
+  name: string;
+  desc: string;
+  catMain: string;
+  catSub: string;
+  condition: string;
+  tags: string[];
+  images: File[];
+}
+
+interface SubCatMap {
+  [key: string]: string[];
+}
+
+const MAX_TAGS = 5;
+const MIN_DESC_LENGTH = 30;
+const MAX_IMAGES = 5;
+
+const SUB_CATS: SubCatMap = {
+  '書籍': ['教科書', '參考書', '小說', '雜誌', '其他'],
+  '科系用品': ['繪圖工具', '實驗器材', '文具', '其他'],
+  '生活用品': ['廚房', '清潔', '收納', '其他'],
+  '3C電子': ['手機', '筆電', '平板', '耳機', '相機', '其他'],
+  '家具家電': ['桌椅', '燈具', '冰箱', '洗衣機', '其他'],
+  '其他': ['其他'],
+};
+
 @Component({
   selector: 'app-launch-product',
   imports: [],
@@ -14,5 +41,7 @@ export class LaunchProductComponent {
   setStep(step: number): void {
     this.activeStep = step;
   }
+
+
 
 }
